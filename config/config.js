83c0183d1c1f7916d3486a6304285f3e0259b3f9
devops-champ <dndarshan2@gmail.com>
2024-12-16 22:03:43 +0530

    integrated mongodb with express.js

---
+++ config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    MONGO_IP: process.env.MONGO_IP || "mongo",
+    MONGO_PORTS: process.env.MONGO_PORTS || 27017,
+    MONGO_USER: process.env.MONGO_USER,
+    MONGO_PASSWORD: process.env.MONGO_PASSWORD 
+
+
+};
--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORTS } = require("./config/config");
 
 const app = express();
 
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORTS}/?authSource=admin`
+
+
+mongoose
+    .connect(mongoURL)
+    .then(() => console.log("successfully connected to Mongo DB"))
+    .catch((e) => console.log(e));
+
 app.get("/", (req, res) => {
     res.send("<h2> Hello I'm your Docker captain</h2>")
-});
+})
 
 const port = process.env.PORT || 3000;
 
