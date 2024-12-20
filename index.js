bc84bd281881bbec37c82be375fff8b6339de5e5
devops-champ <dndarshan2@gmail.com>
2024-12-20 11:44:10 +0530

    implments basic CRUD operation

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORTS } = require("./config/config");
 
+const postRouter = require("./routes/postRoutes")
+
 const app = express();
+
 
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORTS}/?authSource=admin`
 
@@ -12,9 +15,14 @@ mongoose
     .then(() => console.log("successfully connected to Mongo DB"))
     .catch((e) => console.log(e));
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.send("<h2> Hello I'm your Docker captain</h2>")
 })
+
+//localhost:3000/api/v1/post
+app.use("/api/v1/posts", postRouter)
 
 const port = process.env.PORT || 3000;
 
